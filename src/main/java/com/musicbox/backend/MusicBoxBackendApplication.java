3ef2ab1daaa856ec592ee758b0bc8bf92b7cab19
Cai-Glencross <caisglencross@gmail.com>
2024-08-15 20:56:55 -0400

    initial commit

---
+++ src/main/java/com/musicbox/backend/MusicBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musicbox.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicBoxBackendApplication.class, args);
+	}
+
+}
